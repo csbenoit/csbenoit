52af1509b86ed429db9fdf880bfccd77a62fcc3b
Ryan Loomis <rloomis@cvpost104.cv.nrao.edu>
2021-09-15 14:02:15 -0400

    fixed bibtex files

--- paper_dictionary.py
+++ paper_dictionary.py
@@ -2,120 +2,120 @@ paper_dict = {}
 paper_dict['MAPS_I'] = {
             "authors"   : "Karin I. Öberg, Viviana V. Guzmán, Catherine Walsh, Yuri Aikawa, Edwin A. Bergin, Charles J. Law, Ryan A. Loomis, Felipe Alarcón, Sean M. Andrews, Jaehan Bae, Jennifer B. Bergner, Yann Boehler, Alice S. Booth, Arthur D. Bosman, Jenny K. Calahan, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Kenji Furuya, Jane Huang, John D. Ilee, Nicolas T. Kurtovic, Romane Le Gal, Yao Liu, Feng Long, François Ménard, Hideko Nomura, Laura M. Pérez, Chunhua Qi, Kamber R. Schwarz, Anibal Sierra, Richard Teague, Takashi Tsukagoshi, Yoshihide Yamato, Merel L. R. van't Hoff, Abygail R. Waggoner, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) I. Program Overview and Highlights",
-            "arxivID"   : "2019.06268",
+            "arxivID"   : "2109.06268",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06268.pdf"
                       }
 paper_dict['MAPS_II'] = {
             "authors"   : "Ian Czekala, Ryan A. Loomis, Richard Teague, Alice S. Booth, Jane Huang, Gianni Cataldi, John D. Ilee, Charles J. Law, Catherine Walsh, Arthur D. Bosman, Viviana V. Guzmán, Romane Le Gal, Karin I. Öberg, Yoshihide Yamato, Yuri Aikawa, Sean M. Andrews, Jaehan Bae, Edwin A. Bergin, Jennifer B. Bergner, L. Ilsedore Cleeves, François Ménard, Hideko Nomura, Laura M. Pérez, Chunhua Qi, Kamber R. Schwarz, Takashi Tsukagoshi, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) II. CLEAN Strategies for Synthesizing Images of Molecular Line Emission in Protoplanetary Disks",
-            "arxivID"   : "2019.06188",
+            "arxivID"   : "2109.06188",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06188.pdf"
                       }
 paper_dict['MAPS_III'] = {
             "authors"   : "Charles J. Law, Ryan A. Loomis, Richard Teague, Karin I. Öberg, Ian Czekala, Sean M. Andrews, Jane Huang, Yuri Aikawa, Felipe Alarcón, Jaehan Bae, Edwin A. Bergin, Jennifer B. Bergner, Yann Boehler, Alice S. Booth, Arthur D. Bosman, Jenny K. Calahan, Gianni Cataldi, L. Ilsedore Cleeves, Kenji Furuya, Viviana V. Guzmán, John D. Ilee, Romane Le Gal, Yao Liu, Feng Long, François Ménard, Hideko Nomura, Chunhua Qi, Kamber R. Schwarz, Anibal Sierra, Takashi Tsukagoshi, Yoshihide Yamato, Merel L. R. van't Hoff, Catherine Walsh, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) III. Characteristics of Radial Chemical Substructures",
-            "arxivID"   : "2019.06210",
+            "arxivID"   : "2109.06210",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06210.pdf"
                       }
 paper_dict['MAPS_IV'] = {
             "authors"   : "Charles J. Law, Richard Teague, Ryan A. Loomis, Jaehan Bae, Karin I. Öberg, Ian Czekala, Sean M. Andrews, Yuri Aikawa, Felipe Alarcón, Edwin A. Bergin, Jennifer B. Bergner, Alice S. Booth, Arthur D. Bosman, Jenny K. Calahan, Gianni Cataldi, L. Ilsedore Cleeves, Kenji Furuya, Viviana V. Guzmán, Jane Huang, John D. Ilee, Romane Le Gal, Yao Liu, Feng Long, François Ménard, Hideko Nomura, Laura M. Pérez, Chunhua Qi, Kamber R. Schwarz, Daniela Soto, Takashi Tsukagoshi, Yoshihide Yamato, Merel L. R. van't Hoff, Catherine Walsh, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) IV. Emission Surfaces and Vertical Distribution of Molecules",
-            "arxivID"   : "2019.06217",
+            "arxivID"   : "2109.06217",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06217.pdf"
                       }
 paper_dict['MAPS_V'] = {
             "authors"   : "Ke Zhang, Alice S. Booth, Charles J. Law, Arthur D. Bosman, Kamber R. Schwarz, Edwin A. Bergin, Karin I. Öberg, Sean M. Andrews, Viviana V. Guzmán, Catherine Walsh, Chunhua Qi, Merel L. R. van't Hoff, Feng Long, David J. Wilner, Jane Huang, Ian Czekala, John D. Ilee, Gianni Cataldi, Jennifer B. Bergner, Yuri Aikawa, Richard Teague, Jaehan Bae, Ryan A. Loomis, Jenny K. Calahan, Felipe Alarcón, François Ménard, Romane Le Gal, Anibal Sierra, Yoshihide Yamato, Hideko Nomura, Takashi Tsukagoshi, Laura M. Pérez, Leon Trapman, Yao Liu, Kenji Furuya",
             "title"     : "Molecules at Planet Forming Scales (MAPS) V. CO Gas Distributions",
-            "arxivID"   : "2019.06233",
+            "arxivID"   : "2109.06233",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06233.pdf"
                       }
 paper_dict['MAPS_VI'] = {
             "authors"   : "Viviana V. Guzmán, Jennifer B. Bergner, Charles J. Law, Karin I. Öberg, Catherine Walsh, Gianni Cataldi, Yuri Aikawa, Edwin A. Bergin, Ian Czekala, Jane Huang, Sean M. Andrews, Ryan A. Loomis, Ke Zhang, Romane Le Gal, Felipe Alarcón, John D. Ilee, Richard Teague, L. Ilsedore Cleeves, David J. Wilner, Feng Long, Kamber R. Schwarz, Arthur D. Bosman, Laura M. Pérez, François Ménard, Yao Liu",
             "title"     : "Molecules at Planet Forming Scales (MAPS) VI. Distribution of the Small Organics HCN, C2H, and H2CO",
-            "arxivID"   : "2019.06391",
+            "arxivID"   : "2109.06391",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06391.pdf"
                       }
 paper_dict['MAPS_VII'] = {
             "authors"   : "Arthur D. Bosman, Felipe Alarcón, Edwin A. Bergin, Ke Zhang, Merel L. R. van't Hoff, Karin I. Öberg, Viviana V. Guzmán, Catherine Walsh, Yuri Aikawa, Sean M. Andrews, Jennifer B. Bergner, Alice S. Booth, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Kenji Furuya, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Yao Liu, Feng Long, Ryan A. Loomis, François Ménard, Hideko Nomura, Chunhua Qi, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, Yoshihide Yamato, David J. Wilner ",
             "title"     : "Molecules at Planet Forming Scales (MAPS) VII. Substellar O/H and C/H and Superstellar C/O in Planet-feeding Gas",
-            "arxivID"   : "2019.06221",
+            "arxivID"   : "2109.06221",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06221.pdf"
                       }
 paper_dict['MAPS_VIII'] = {
             "authors"   : "Felipe Alarcón, Arthur D. Bosman, Edwin A. Bergin, Ke Zhang, Richard Teague, Jaehan Bae, Yuri Aikawa, Sean M. Andrews, Alice S. Booth, Jenny K. Calahan, Gianni Cataldi, Ian Czekala, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Yao Liu, Feng Long, François Ménard, Karin I. Öberg, Kamber R. Schwarz, Merel L. R. van't Hoff, Catherine Walsh, David J. Wilner",
             "title"     : "Molecules at Planet Forming Scales (MAPS) VIII. CO Gap in AS 209&mdash;Gas Depletion or Chemical Processing?",
-            "arxivID"   : "2019.06263",
+            "arxivID"   : "2109.06263",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06263.pdf"
                       }
 paper_dict['MAPS_IX'] = {
             "authors"   : "John D. Ilee, Catherine Walsh, Alice S. Booth, Yuri Aikawa, Sean M. Andrews, Jaehan Bae, Edwin A. Bergin, Jennifer B. Bergner, Arthur D. Bosman, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Viviana V. Guzmán, Jane Huang, Charles J. Law, Romane Le Gal, Ryan A. Loomis, François Ménard, Hideko Nomura, Karin I. Öberg, Chunhua Qi, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, David J. Wilner, Yoshihide Yamato, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) IX. Distribution and Properties of the Large Organic Molecules HC3N, CH3CN, and c-C3H2",
-            "arxivID"   : "2019.06319",
+            "arxivID"   : "2109.06319",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06319.pdf"
                       }
 paper_dict['MAPS_X'] = {
             "authors"   : "Gianni Cataldi, Yoshihide Yamato, Yuri Aikawa, Jennifer B. Bergner, Kenji Furuya, Viviana V. Guzmán, Jane Huang, Ryan A. Loomis, Chunhua Qi, Sean M. Andrews, Edwin A. Bergin, Alice S. Booth, Arthur D. Bosman, L. Ilsedore Cleeves, Ian Czekala, John D. Ilee, Charles J. Law, Romane Le Gal, Yao Liu, François Ménard, Hideko Nomura, Karin I. Öberg, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, Catherine Walsh, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) X. Studying Deuteration at High Angular Resolution toward Protoplanetary Disks",
-            "arxivID"   : "2019.06462",
+            "arxivID"   : "2109.06462",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06462.pdf"
                       }
 paper_dict['MAPS_XI'] = {
             "authors"   : "Jennifer B. Bergner, Karin I. Öberg, Viviana V. Guzmán, Charles J. Law, Ryan A. Loomis, Gianni Cataldi, Arthur D. Bosman, Yuri Aikawa, Sean M. Andrews, Edwin A. Bergin, Alice S. Booth, L. Ilsedore Cleeves, Ian Czekala, Jane Huang, John D. Ilee, Romane Le Gal, Feng Long, Hideko Nomura, François Ménard, Chunhua Qi, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, Catherine Walsh, David J. Wilner, Yoshihide Yamato",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XI. CN and HCN as Tracers of Photochemistry in Disks",
-            "arxivID"   : "2019.06694",
+            "arxivID"   : "2109.06694",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06694.pdf"
                       }
 paper_dict['MAPS_XII'] = {
             "authors"   : "Romane Le Gal, Karin I. Öberg, Richard Teague, Ryan A. Loomis, Charles J. Law, Catherine Walsh, Edwin A. Bergin, François Ménard, David J. Wilner, Sean M. Andrews, Alice S. Booth, Yuri Aikawa, Gianni Cataldi, Jennifer B. Bergner, Arthur D. Bosman, L. Ilsedore Cleeves, Ian Czekala, Kenji Furuya, Viviana V. Guzmán, Jane Huang, John D. Ilee, Hideko Nomura, Chunhua Qi, Kamber R. Schwarz, Takashi Tsukagoshi, Yoshihide Yamato, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XII. Inferring the C/O and S/H Ratios in Protoplanetary Disks with Sulfur Molecules",
-            "arxivID"   : "2019.06286",
+            "arxivID"   : "2109.06286",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06286.pdf"
                       }
 paper_dict['MAPS_XIII'] = {
             "authors"   : "Yuri Aikawa, Gianni Cataldi, Yoshihide Yamato, Ke Zhang, Alice S. Booth, Kenji Furuya, Sean M. Andrews, Jaehan Bae, Edwin A. Bergin, Jennifer B. Bergner, Arthur D. Bosman, L. Ilsedore Cleeves, Ian Czekala, Viviana V. Guzman, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Ryan A. Loomis, François Ménard, Hideko Nomura, Karin I. Öberg, Chunhua Qi, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, Catherine Walsh, David J. Wilner",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XIII, HCO+ and Disk Ionization Structure",
-            "arxivID"   : "2019.06419",
+            "arxivID"   : "2109.06419",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06419.pdf"
                       }
 paper_dict['MAPS_XIV'] = {
             "authors"   : "Anibal Sierra, Laura M. Pérez, Viviana V. Guzmán, Ke Zhang, Alice S. Booth, Arthur D. Bosman, Catherine Walsh, Charles J. Law, Chunhua Qi, David J. Wilner, Feng Long, François Ménard, Gianni Cataldi, Ian Czekala, Jaehan Bae, Jane Huang, Jennifer B. Bergner, John D. Ilee, Karin I. Öberg, Myriam Benisty,  Richard Teague, Romane Le Gal, Ryan A. Loomis, Sean M. Andrews, Takashi Tsukagoshi, Yao Liu, Yoshihide Yamato, Yuri Aikawa",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XIV. Revealing Disk Substructures in Multiwavelength Continuum Emission",
-            "arxivID"   : "2019.06433",
+            "arxivID"   : "2109.06433",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06433.pdf"
                       }
 paper_dict['MAPS_XV'] = {
             "authors"   : "Arthur D. Bosman, Edwin A. Bergin, Ryan A. Loomis, Sean M. Andrews, Merel L. R. van't Hoff, Richard Teague, Karin I. Öberg, Viviana V. Guzmán, Catherine Walsh, Yuri Aikawa, Felipe Alarcón, Jaehan Bae, Jennifer B. Bergner, Alice S. Booth, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Yao Liu, Feng Long, François Ménard, Hideko Nomura, Laura M. Pérez, Chunhua Qi, Kamber R. Schwarz, Anibal Sierra, Takashi Tsukagoshi, Yoshihide Yamato, David J. Wilner, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XV. Tracing Protoplanetary Disk Structure within 20 au",
-            "arxivID"   : "2019.06223",
+            "arxivID"   : "2109.06223",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06223.pdf"
                       }
 paper_dict['MAPS_XVI'] = {
             "authors"   : "Alice S. Booth, Benoît Tabone, John D. Ilee, Catherine Walsh, Yuri Aikawa, Sean M. Andrews, Jaehan Bae, Jennifer B. Bergner, Arthur D. Bosman, Jenny K. Calahan, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Viviana V. Guzmán, Jane Huang, Charles J. Law, Romane Le Gal, Feng Long, Ryan A. Loomis, François Ménard, Hideko Nomura, Karin I. Öberg,  Chunhua Qi, Kamber R. Schwarz, Richard Teague, Takashi Tsukagoshi, David J. Wilner, Yoshihide Yamato, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XVI. Characterizing the Impact of the Molecular Wind on the Evolution of the HD 163296 System",
-            "arxivID"   : "2019.06586",
+            "arxivID"   : "2109.06586",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06586.pdf"
                       }
 paper_dict['MAPS_XVII'] = {
             "authors"   : "Jenny K. Calahan, Edwin A. Bergin, Ke Zhang, Kamber R. Schwarz, Karin I. Öberg, Viviana V. Guzmán, Catherine Walsh, Yuri Aikawa, Felipe Alarcón, Sean M. Andrews, Jaehan Bae, Jennifer B. Bergner, Alice S. Booth, Arthur D. Bosman, Gianni Cataldi, Ian Czekala, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Feng Long, Ryan A. Loomis, François Ménard, Chunhua Qi, Richard Teague, Merel L. R. van't Hoff, David J. Wilner, Yoshihide Yamato",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XVII. Determining the 2D Thermal Structure of the HD 163296 Disk",
-            "arxivID"   : "2019.06202",
+            "arxivID"   : "2109.06202",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06202.pdf"
                       }
 paper_dict['MAPS_XVIII'] = {
             "authors"   : "Richard Teague, Jaehan Bae, Yuri Aikawa, Sean M. Andrews, Edwin A. Bergin, Jennifer B. Bergner, Yann Boehler, Alice S. Booth, Arthur D. Bosman, Gianni Cataldi, Ian Czekala, Viviana V. Guzmán, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Feng Long, Ryan A. Loomis, François Ménard, Karin I. Öberg, Laura M. Pérez, Kamber R. Schwarz, Anibal Sierra, Catherine Walsh, David J. Wilner, Yoshihide Yamato, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XVIII. Kinematic Substructures in the Disks of HD 163296 and MWC 480",
-            "arxivID"   : "2019.06218",
+            "arxivID"   : "2109.06218",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06218.pdf"
                       }
 paper_dict['MAPS_XIX'] = {
             "authors"   : "Jane Huang, Edwin A. Bergin, Karin I. Öberg, Sean M. Andrews, Richard Teague, Charles J. Law, Paul Kalas, Yuri Aikawa, Jaehan Bae, Jennifer B. Bergner, Alice S. Booth, Arthur D. Bosman, Jenny K. Calahan, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, John D. Ilee, Romane Le Gal, Viviana V. Guzmán, Feng Long, Ryan A. Loomis, François Ménard, Hideko Nomura, Chunhua Qi, Kamber R. Schwarz, Takashi Tsukagoshi, Merel L. R. van't Hoff, Catherine Walsh, David J. Wilner, Yoshihide Yamato, Ke Zhang",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XIX. Spiral Arms, a Tail, and Diffuse Structures Traced by CO around the GM Aur Disk",
-            "arxivID"   : "2019.06224",
+            "arxivID"   : "2109.06224",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06224.pdf"
                       }
 paper_dict['MAPS_XX'] = {
             "authors"   : "Kamber R. Schwarz, Jenny K. Calahan, Ke Zhang, Felipe Alarcón, Yuri Aikawa, Sean M. Andrews, Jaehan Bae, Edwin A. Bergin, Alice S. Booth, Arthur D. Bosman, Gianni Cataldi, L. Ilsedore Cleeves, Ian Czekala, Jane Huang, John D. Ilee, Charles J. Law, Romane Le Gal, Yao Liu, Feng Long, Ryan A. Loomis, Enrique Macías,  Melissa McClure, François Ménard, Karin I. Öberg, Richard Teague, Ewine van Dishoeck, Catherine Walsh, David J. Wilner",
             "title"     : "Molecules at Planet Forming Scales (MAPS) XX. The Massive Disk around GM Aurigae",
-            "arxivID"   : "2019.06228",
+            "arxivID"   : "2109.06228",
             "arxivURL"  : "https://arxiv.org/pdf/2109.06228.pdf"
                       }
